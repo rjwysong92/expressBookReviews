94a473e9ea69a2b3dd4a3b234178af3137708595
rjwysong92 <rjbrewer92@gmail.com>
2023-09-19 13:09:28 -0400

    Update230919.1300

--- final_project/index.js
+++ final_project/index.js
@@ -11,24 +11,22 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    if (!username || !password) {
-        return res.status(404).json({message: "Error logging in"});
-    }
-    if (authenticatedUser(username,password)) {
-        let accessToken = jwt.sign({
-            data: password
-        }, 'access', { expiresIn: 60 });
-
-        req.session.authorization = {
-            accessToken,username
-        }
-        return res.status(200).send("User successfully logged in");
-    } else {
-        return res.status(208).json({message: "Invalid Login. CHeck username and password"});
-    }});
+    let token = req.session.authorization;
+    if(token) {
+        token = token['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "Customer not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "Customer not logged in"})
+     }
+ });
  
 const PORT =5000;
 
