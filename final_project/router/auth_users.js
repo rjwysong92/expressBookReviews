3a96f4e5105270e45e2c45453e0f9cdceba32d65
rjwysong92 <rjbrewer92@gmail.com>
2023-09-26 15:39:09 -0400

    UpdateNodeJsProject9.26.23-1536

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -29,29 +29,51 @@ const authenticatedUser = (username,password)=>{
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (!username || !password) {
-        return res.status(404).json({message: "Error logging in"});
+    const user = req.body.regd_users;
+    if (!user) {
+        return res.status(404).json({message: "Body Empty"});
     }
-   if (authenticatedUser(username,password)) {
-      let accessToken = jwt.sign({
-        data: password
-      }, 'access', { expiresIn: 60 });
+    let accessToken = jwt.sign({
+        data: user
+      }, 'access', { expiresIn: 60 * 60 });
+
       req.session.authorization = {
-        accessToken,username
+        accessToken
     }
     return res.status(200).send("User successfully logged in");
-    } else {
-      return res.status(208).json({message: "Invalid Login. Check username and password"});
-    }});
-
+});
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let filtered_book = books[isbn];
+  if (filtered_book) {
+      let review = req.query.review;
+      let reviewer = req.session.authorization['username'];
+      if(review) {
+          filtered_book['reviews'][reviewer] = review;
+          books[isbn] = filtered_book;
+    }
+    res.send(`The review for the book with ISBN ${isbn} has been added/updated!`);
+  } 
+  else{
+      res.send("Unable to find this ISBN!");
+  }
 });
+
+// Delete a book reivew done only by specific authorized user
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.books[isbn];
+    let reviewer = req.session.authorization['username'];
+    if (filtered_review[reviewer]){
+        delete filtered_review[reviewer];
+        res.send(`Reviews for the ISBN ${isbn} posted by the user ${reviewer} deleted.`);
+    }
+    else{
+        res.send("can't delete, as this review has been posted by a different user");
+    }
+    });
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
